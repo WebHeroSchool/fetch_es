2b7d8596506c3a415088df4d8f45b10a7b267f90
Evgeniya Skripaleva <e.skripaleva@gmail.com>
2020-01-15 16:53:44 +0300

    task complited

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-fetch ('https://api.github.com/users/defunkt')
+let url = window.location.toString();
+
+let getUsername = (url) => {
+    let urlArray = url.split('=');
+    let userName = urlArray[1];
+    if (userName == undefined){
+        userName = 'skripaleva';
+    }
+    return userName
+}
+
+let name = getUsername(url)
+
+fetch('https://api.github.com/users/' + name)
     .then(res => res.json())
-    .then(json => console.log(json.bio))
-    .catch(err => console.log(err));
+    .then(json => {
+        let avatar = json.avatar_url;
+        let name = json.login;
+        let bio = json.bio;
+        let profile =json.html_url;
+        if (name) {
+
+            let createAvatar = () => {
+                let newAvatar = document.createElement('img');
+                newAvatar.src = avatar;
+  //              let addString = document.createElement('br');
+                document.body.appendChild(newAvatar);
+  //              document.body.appendChild(addString);
+            }
+
+            let createBio = () => {
+                let newBio = document.createElement('p');
+                newBio.innerHTML = bio;
+                document.body.appendChild(newBio);
+            }
+
+            let createProfile = () => {
+                let elementForLink = document.createElement('a');
+                let elementForHeader = document.createElement('h2');
+                elementForHeader.innerText = name;
+                elementForLink.href = profile;
+                document.body.appendChild(elementForLink);
+                elementForLink.appendChild(elementForHeader);
+            }
+
+            createProfile();
+            createBio();
+            createAvatar();
+        }
+        else {
+            alert(' Профиль не найден')
+        }
+    })
+
+    .catch(err => alert(err + ' Профиль не найден'));

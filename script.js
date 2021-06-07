3b981759bf3523ece4e9dd7975b017876fbc8182
Evgeniya <evgeniya_ignaten@mail.ru>
2021-06-07 11:31:41 +0300

    some changes

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let url = window.location.toString();
 let getUsername = (url) => {
     let urlArray = url.split('=');
     let userName = urlArray[1];
-    if (userName == undefined){
+    if (userName == undefined || userName.length > 15){
         userName = 'skripaleva';
     }
     return userName
-}
+};
 
 let name = getUsername(url);
 
@@ -20,12 +20,14 @@ let getNowDate = new Promise((resolve, reject) => {
 let getUserData = fetch('https://api.github.com/users/' + name);
 
 Promise.all([getUserData, getNowDate])
-    .then(([ourUserData, ourNowDate]) => {
-        userData = ourUserData;
-        currentDate = ourNowDate;
+
+    .then((res) => {
+        userData = res[0];
+        currentDate = res[1];
     })
 
-    .then(res => userData.json())
+    .then(() => userData.json())
+
     .then(userInfo => {
         let avatar = userInfo.avatar_url;
         let name = userInfo.login;
@@ -38,13 +40,13 @@ Promise.all([getUserData, getNowDate])
                 let addString = document.createElement('br');
                 document.body.appendChild(newAvatar);
                 document.body.appendChild(addString);
-            }
+            };
 
             let createBio = () => {
                 let newBio = document.createElement('p');
                 newBio.innerHTML = bio;
                 document.body.appendChild(newBio);
-            }
+            };
 
             let createProfile = () => {
                 let elementForLink = document.createElement('a');
@@ -53,13 +55,13 @@ Promise.all([getUserData, getNowDate])
                 elementForLink.href = profile;
                 document.body.appendChild(elementForLink);
                 elementForLink.appendChild(elementForHeader);
-            }
+            };
 
             let createDate = () => {
                 let newCurrentDate = document.createElement('p');
                 newCurrentDate.innerHTML = currentDate;
                 document.body.appendChild(newCurrentDate);
-            }
+            };
 
             let elementForPreloader = document.getElementById('preload');
             elementForPreloader.classList.add('hidden');
